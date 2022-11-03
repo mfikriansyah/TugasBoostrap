c3b089c39bf89ce3e0ae02aae6f267d7b0074bcd
mfikriansyah <mfikri270601@gmail.com>
2022-11-03 10:45:57 +0700

    Submit tugas boostrap selesai

---
+++ asset/latihan-main/latihan-main/js/script.js
@@ -0,0 +1,46 @@
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
+const elementclose = document.getElementsByClassName('.overlay');
+for(let i=0;i<elementclose;i++){
+    let url=window.location.href.substr(0,window.location.href.indexOf('#'))
+    window.location=url;
+}
